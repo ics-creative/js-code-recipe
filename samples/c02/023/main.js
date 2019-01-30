11e3e5fcaa9303725cb906907d14debccadc85ab
ics-kano <takeshi.kano@ics-web.jp>
2019-01-30 21:46:10 +0900

    2章023：ランダムな数取得のコメントを更新

--- samples/c02/023/main.js
+++ samples/c02/023/main.js
@@ -6,7 +6,7 @@ document.querySelector('.button').addEventListener('click', onClickButton);
 
 /** ボタンをクリックする度に、長方形のグラデーション色を変える */
 function onClickButton() {
-  // 0~360の間のランダムな数を取得する
+  // 0~359の間のランダムな数を取得する
   const randomHue = Math.trunc(Math.random() * 360);
   // グラデーションの開始色と終了色を決定
   const randomColorStart = `hsl(${randomHue}, 100%, 50%)`;
